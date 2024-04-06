a5936f649ca319c85f320e7b23547167e578bff3
RandomJapaneseDude <83206060+inoue-773@users.noreply.github.com>
2024-04-06 13:47:49 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -368,7 +368,7 @@ async def flecity(ctx):
         embed.set_thumbnail(url="https://i.imgur.com/sK2BAAO.png")
         embed.set_footer(text="Powered By NickyBoy", icon_url="https://i.imgur.com/QfmDKS6.png")
 
-        await ctx.followup.send(embed=embed)
+        await ctx.followup.send(embed=embed, ephemeral=True)
     except Exception as e:
         print(f"Error occurred while pinging the server: {str(e)}")
         logging.error(f"Error occurred while pinging the server: {str(e)}")
